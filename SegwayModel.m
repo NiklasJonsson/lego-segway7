ddd7ef0a8a05d606a27d7662c31492f73420b3a4
Marcus Kindberg <tpi12mki@philon-05.control.lth.se>
2016-04-06 11:56:15 +0200

    more changes to matlab variables

--- SegwayModel.m
+++ SegwayModel.m
@@ -1,21 +1,23 @@
 M = 0.1; %Mass of cart
-m = 0.7; %Mass of pendulum
+m = 0.4; %Mass of pendulum
 b = 0.1; %Cart friction
 g = 9.8;
 l = 0.15; %length of pendulum
 s = tf('s');
 
 gamma=0; %rotational friction
-J=0; %mass moment of inertia of the pendulum 
+J=0.08; %mass moment of inertia of the pendulum 
 Jt=J+m*l*l;
 h=0.05; %sampling interval
-p=[1+1i 1-1i]; %continous time poles
+p=[-1+1i -1-1i]; %continous time poles
+pd=exp(p*h);
 
 A=[0 1; m*g*l/Jt -gamma/Jt];
-B=[0;1i/Jt];
+B=[0;l/Jt];
 C=[1 0];
 D=0;
 
 G=ss(A, B, C, D);
 H=c2d(G, h);
-L=place(A, B, p); %place poles in continous time
+L=place(H.a, H.b, pd); %place poles in continous time
+lr=inv(C/(eye(2)-H.a+H.b*L)*H.b);

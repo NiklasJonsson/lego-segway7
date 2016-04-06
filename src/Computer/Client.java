42cbb08fb6c4672a890547416cb0b3ce3db834d0
eriksoderberg15 <dat12eso@student.lu.se>
2016-04-06 10:50:01 +0200

    added communication classes: server/client

---
+++ src/Computer/Client.java
@@ -0,0 +1,9 @@
+package Computer;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Client {
+
+}
---
+++ src/Segway/Server.java
@@ -0,0 +1,9 @@
+package Segway;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Server {
+
+}

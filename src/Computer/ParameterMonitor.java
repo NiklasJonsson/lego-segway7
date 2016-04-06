d5a1a90819a8cfc94fa33e9c31e8c71ea8f74744
eriksoderberg15 <dat12eso@student.lu.se>
2016-04-06 10:05:36 +0200

    Pushing empty classes

---
+++ src/Computer/ParameterMonitor.java
@@ -0,0 +1,12 @@
+package Computer;
+
+public class ParameterMonitor {
+	public double K;
+	public double Ti;
+	public double Tr;
+	public double Td;
+	public double N;
+	public double Beta;
+	public double H;
+	public boolean integratorOn;
+}

9120cab50500800ce57a5cad24fce81c31de9b05
Marcus Kindberg <tpi12mki@philon-07.control.lth.se>
2016-04-12 11:23:49 +0200

    Added moment of inertia and integral action

--- SegwayModel.m
+++ SegwayModel.m
@@ -1,13 +1,13 @@
 M = 0.1; %Mass of cart
 m = 0.4; %Mass of pendulum
-b = 0.1; %Cart friction
+%b = 0.1; %Cart friction
 g = 9.8;
 l = 0.15; %length of pendulum
-s = tf('s');
+w = 0.1;
 
 gamma=0; %rotational friction
-J=0.08; %mass moment of inertia of the pendulum 
-Jt=J+m*l*l;
+J=m*(l^2+w^2)/12+(l/2)^2*m; %mass moment of inertia of the pendulum 
+Jt=J+m*l^2;
 h=0.05; %sampling interval
 p=[-1+1i -1-1i]; %continous time poles
 pd=exp(p*h);
@@ -21,3 +21,13 @@ G=ss(A, B, C, D);
 H=c2d(G, h);
 L=place(H.a, H.b, pd); %place poles in continous time
 lr=inv(C/(eye(2)-H.a+H.b*L)*H.b);
+
+
+%With integral action
+p=[0.6+2*1i, 0.6-2*1i, 0.55];
+
+Ae=[H.a H.b;zeros(1, 2) 1];
+Be=[H.b;0];
+Ce=[C 0];
+
+K=place(Ae', Ce', p);

da50c18119c4abe61594846fadb61aa1e4f1e1fb
eriksoderberg15 <dat12eso@student.lu.se>
2016-04-14 14:48:15 +0200

    Added an r parameter in parameters class + GUI

--- src/computer/ParameterGUI.java
+++ src/computer/ParameterGUI.java
@@ -1,9 +1,10 @@
 package computer;
+ import utility.*;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,12 +14,14 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class ParameterGUI {
-	private JTextField l1Field;
-	private JTextField l2Field;
-	private JTextField lrField;
 	private JTextField k1Field;
 	private JTextField k2Field;
+	private JTextField l1Field;
+	private JTextField l2Field;
 	private JTextField rField;
+	private JTextField lrField;
+	private JTextField integralInputField;
+	private JTextField kvField;
 
 	public void createAndShow() {
 		JFrame frame = new JFrame();
@@ -38,21 +41,32 @@ public class ParameterGUI {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				double l1, l2, lr, k1, k2, r = 0;
+				double l1, l2, lr, k1, k2, r, integralInput, kv = 0;
+				boolean integralAction;
 				try {
+					k1 = Double.parseDouble(k1Field.getText());
+					k2 = Double.parseDouble(k2Field.getText());
 					l1 = Double.parseDouble(l1Field.getText());
 					l2 = Double.parseDouble(l2Field.getText());
 					lr = Double.parseDouble(lrField.getText());
-					k1 = Double.parseDouble(k1Field.getText());
-					k2 = Double.parseDouble(k2Field.getText());
 					r = Double.parseDouble(rField.getText());
-					
-					System.out.println(l1);
-					System.out.println(l2);
-					System.out.println(lr);
-					System.out.println(k1);
-					System.out.println(k2);
-					System.out.println(r);
+					kv = Double.parseDouble(kvField.getText());
+					integralInput = Double.parseDouble(integralInputField.getText());
+					if(integralInput<=0){
+						integralAction = false;
+					}else{
+						integralAction = true;
+					}
+					System.out.println("l1:" + l1);
+					System.out.println("l2" + l2);
+					System.out.println("lr" + lr);
+					System.out.println("k1" + k1);
+					System.out.println("k2" +k2);
+					System.out.println("r" +r);
+					System.out.println("integralaction" +integralAction);
+					System.out.println("kv" +kv);
+				
+					Parameters p = new Parameters(k1, k2, l1, l2, lr, kv, integralAction, r);
 					
 				} catch (NumberFormatException err) {
 					JOptionPane.showMessageDialog(null, "Please enter numbers in all fields", "Error",
@@ -64,7 +78,7 @@ public class ParameterGUI {
 	}
 
 	private JPanel paramPane() {
-		JPanel pane = new JPanel(new GridLayout(6, 2));
+		JPanel pane = new JPanel(new GridLayout(8, 2));
 
 		l1Field = new JTextField();
 		pane.add(new JLabel("L1:"));
@@ -89,6 +103,14 @@ public class ParameterGUI {
 		rField = new JTextField();
 		pane.add(new JLabel("r:"));
 		pane.add(rField);
+		
+		integralInputField = new JTextField();
+		pane.add(new JLabel("Integral action(0=no, 1=yes):"));
+		pane.add(integralInputField);
+		
+		kvField = new JTextField();
+		pane.add(new JLabel("kv:"));
+		pane.add(kvField);
 
 		return pane;
 	}

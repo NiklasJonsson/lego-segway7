615ebb4f44b1658e3b3e2a481b12a5722d8af4c6
eriksoderberg15 <dat12eso@student.lu.se>
2016-04-14 14:03:01 +0200

    Fixat lite i GUI

--- src/computer/ParameterGUI.java
+++ src/computer/ParameterGUI.java
@@ -16,6 +16,9 @@ public class ParameterGUI {
 	private JTextField l1Field;
 	private JTextField l2Field;
 	private JTextField lrField;
+	private JTextField k1Field;
+	private JTextField k2Field;
+	private JTextField rField;
 
 	public void createAndShow() {
 		JFrame frame = new JFrame();
@@ -35,11 +38,22 @@ public class ParameterGUI {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				double l1 = 0, l2 = 0, lr = 0;
+				double l1, l2, lr, k1, k2, r = 0;
 				try {
 					l1 = Double.parseDouble(l1Field.getText());
 					l2 = Double.parseDouble(l2Field.getText());
 					lr = Double.parseDouble(lrField.getText());
+					k1 = Double.parseDouble(k1Field.getText());
+					k2 = Double.parseDouble(k2Field.getText());
+					r = Double.parseDouble(rField.getText());
+					
+					System.out.println(l1);
+					System.out.println(l2);
+					System.out.println(lr);
+					System.out.println(k1);
+					System.out.println(k2);
+					System.out.println(r);
+					
 				} catch (NumberFormatException err) {
 					JOptionPane.showMessageDialog(null, "Please enter numbers in all fields", "Error",
 							JOptionPane.ERROR_MESSAGE);
@@ -50,7 +64,7 @@ public class ParameterGUI {
 	}
 
 	private JPanel paramPane() {
-		JPanel pane = new JPanel(new GridLayout(3, 2));
+		JPanel pane = new JPanel(new GridLayout(6, 2));
 
 		l1Field = new JTextField();
 		pane.add(new JLabel("L1:"));
@@ -63,6 +77,18 @@ public class ParameterGUI {
 		lrField = new JTextField();
 		pane.add(new JLabel("Lr:"));
 		pane.add(lrField);
+		
+		k1Field = new JTextField();
+		pane.add(new JLabel("k1:"));
+		pane.add(k1Field);
+		
+		k2Field = new JTextField();
+		pane.add(new JLabel("k2:"));
+		pane.add(k2Field);
+		
+		rField = new JTextField();
+		pane.add(new JLabel("r:"));
+		pane.add(rField);
 
 		return pane;
 	}

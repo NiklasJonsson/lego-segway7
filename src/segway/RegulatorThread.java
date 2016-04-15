cc4929535347f3d90339223d40a457920ca69867
Niklas Jonsson <niklasandersjonsson@gmail.com>
2016-04-15 09:43:21 +0200

    Fixed motors api

--- src/segway/RegulatorThread.java
+++ src/segway/RegulatorThread.java
@@ -9,12 +9,12 @@ public class RegulatorThread extends Thread {
 	private final double[] b = new double[2];
 	private final double[] c = new double[2];
 	private RegulatorMonitor rm;
-	private Motor m;
+	private Motors m;
 	private Accelerometer acc;
-	
-	public RegulatorThread(RegulatorMonitor rm){
-		this.rm=rm;
-		m = new Motor();
+
+	public RegulatorThread(RegulatorMonitor rm) {
+		this.rm = rm;
+		m = new Motors();
 		acc = new Accelerometer();
 	}
 
@@ -35,23 +35,26 @@ public class RegulatorThread extends Thread {
 
 		double e = 0;
 		double v = 0;
+		int i = 1;
 		while (!Thread.interrupted()) {
 			double[] data = acc.read();
 			double accel = data[1];
 			SegwayMain.printToScreen("0: " + data[0], "1: " + data[1], "2: " + data[2]);
-			 // TODO add more variables in signals? (v, r...)
+			// TODO add more variables in signals? (v, r...)
 			Signals s = rm.getSignals();
 
 			u = lr * r + s.l1 * s.x1 + s.l2 * s.x2 - v;
 			e = y - x2;
+			e=0;
 			x1 = a[0][0] * s.x1 + a[0][1] * s.x2 + b[0] * u + k1 * e;
 			x2 = a[1][0] * s.x1 + a[1][1] * s.x2 + b[1] * u + k2 * e;
 
 			v = v + kv * e;
 
 			// TODO write signals
-			m.move(7000);
-			
+			m.sendSignal(u);
+			i *= -1;
+
 			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e1) {

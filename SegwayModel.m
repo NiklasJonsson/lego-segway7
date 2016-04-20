30d85cab6f73fcc19f58e1d1ab6046b05b418c02
Maurax <marcus3111@gmail.com>
2016-04-20 21:01:21 +0200

    Might have found a better pole placement for K

--- SegwayModel.m
+++ SegwayModel.m
@@ -11,7 +11,7 @@ h=0.05; %sampling interval
 p=[-1+1i -1-1i]; %continous time poles
 pd=exp(p*h);
 
-A=[0 1; m*g*l/t -gamma/J]; %I think A(2, 1) is too big, probably because Jt is too small. mgl=0.5, but then we divide by Jt, which is small, and suddenly it's 37J
+A=[0 1; m*g*l/J -gamma/J]; %I think A(2, 1) is too big, probably because Jt is too small. mgl=0.5, but then we divide by Jt, which is small, and suddenly it's 37
 B=[0;l/J]; %B(2) would also be more resonable if Jt was bigger
 C=[1 0];
 D=0;
@@ -23,7 +23,7 @@ lr=inv(C/(eye(2)-H.a+H.b*L)*H.b);
 
 
 %With integral action
-p=[0.6+1i, 0.6-1i, 0.55]; %We might be placiing the poles wrong, changing to 1i instead of 2 more than halved the K:s
+p=[0.8+0.1*1i, 0.8-0.1*1i, 0.85]; %We might be placiing the poles wrong, changing to 1i instead of 2 more than halved the K:s
 
 Ae=[H.a H.b;zeros(1, 2) 1];
 Be=[H.b;0];
